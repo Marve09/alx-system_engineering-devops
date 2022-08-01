6162779439401cce29042080e64051ff7782a87e
Marve09 <marvemarcel@gmail.com>
2022-08-01 06:44:04 +0100

    My work

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,12 +1,18 @@
+/*
+ * File: 102-zombie.c
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
-/**
- * infinite_while - creates an infinite loop
- * Return: 0
- */
 
+/**
+ * infinite_while - Run an infinite while loop.
+ *
+ * Return: Always 0.
+ */
 int infinite_while(void)
 {
 	while (1)
@@ -15,23 +21,31 @@ int infinite_while(void)
 	}
 	return (0);
 }
+
 /**
- * main - creates 5 zombies process
- * Return: 0
+ * main - Creates five zombie processes.
  *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int z;
-	pid_t zombie;
+	pid_t pid;
+	char count = 0;
 
-	for (z = 0; z < 5; z++)
+	while (count < 5)
 	{
-		zombie = fork();
-		if (zombie == 0)
-			return (0);
-		printf("Zombie process created, PID: %d\n", zombie);
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			count++;
+		}
+		else
+			exit(0);
 	}
+
 	infinite_while();
-	return (0);
+
+	return (EXIT_SUCCESS);
 }
--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,2 +0,0 @@
-# Fixing error 500 in apache server with puppet
-exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
